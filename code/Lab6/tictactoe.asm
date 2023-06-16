ab4d0ca834eff55b8a342e61184bfdf213f5b11b
Lukasz Fabia <114498264+lukaszfabia@users.noreply.github.com>
2023-06-16 16:26:47 +0200

    porządnie zaczynam

--- code/Lab6/tictactoe.asm
+++ code/Lab6/tictactoe.asm
@@ -1,17 +1,17 @@
-.data
-# zmienne globalne
-player_name: .space 20
-amount_rounds: .word 0
-grid: .space 9
-results_for_human: .word 0
-results_for_computer: .word 0
-position: .word 0
-choiced: .byte 1
-xoro: .byte 'x', 'o'  # Przechowuje znaki 'x' i 'o'
-computer_mark: .byte 1 # Przechowuje znak komputera
-corners: .word 0, 2, 6, 8 # Przechowuje indeksy rogów planszy
+.data 
+# tablice/ciagi znakow
+player_name: .space 30
+grid: .space 36
+sign_set: .byte 'x', 'o'
+choiced_sign: .space 1
+computer_sign: .space 1
 
-# prompty dla gracza
+# zmienne int
+amount_rounds: .word 0
+human_score: .word 0
+computer_score: .word 0
+
+# napisy
 computer: .asciiz "Komputer"
 prompt_player_name: .asciiz "Podaj swoje imie: "
 prompt_amount_rounds: .asciiz "Podaj ilosc rund: "
@@ -22,23 +22,87 @@ draw_msg: .asciiz "Remis!"
 end_msg: .asciiz "Koniec gry!"
 round_msg: .asciiz "Runda: "
 choice_msg: .asciiz "Wybierz swoj znak 'x' albo 'o': "
-divider: .asciiz "-----------------------------------\n"
+divider: .asciiz "\n-----------------------------------\n\n"
 computer_msg: .asciiz "Komputer wybral pozycje: "
+start_grid: .asciiz "1 | 2 | 3\n----------\n4 | 5 | 6\n----------\n7 | 8 | 9\n"
 
+
+# zarezerwowane rejestry: s0
 
 .text
 main:
-    # pobranie imienia gracza
-    li $v0, 4
+	li $v0, 4
     la $a0, prompt_player_name
     syscall
 
     li $v0, 8
     la $a0, player_name
+	li $a1, 30
     syscall
 
-enter_amount_rounds:
-    # pobranie ilosci rund
+	# pobranie ilosci rund
+	jal get_rounds
+
+	# pobranie znaku od gracza
+	jal get_sign
+
+	# ustawienie znaku komputera
+	jal get_computer_sign
+
+
+	addi $s0, $zero, 1 # ustawienie numeru rundy na 1
+	lw $s1, amount_rounds # wczytanie ilosci rund
+	game_loop:
+		beq $s0, $s1, exit # jesli numer rundy jest rowny ilosci rund to zakoncz gre
+
+		# wypisanie podzialki
+		jal print_divider
+
+		# wypisanie planszy
+		jal print_grid
+		addi $s0, $s0, 1 # zwiekszenie numeru rundy o 1
+	j game_loop
+
+
+exit:
+	li $v0, 10
+	syscall
+
+
+get_computer_sign:
+	lb $t0, choiced_sign
+	beq $t0, 120, set_o # jesli wybrano 'x' to ustaw znak komputera na 'o'
+	beq $t0, 111, set_x # jesli wybrano 'o' to ustaw znak komputera na 'x'
+	set_o:
+		li $t0, 111
+		sb $t0, computer_sign
+		jr $ra
+	
+	set_x:
+		li $t0, 120
+		sb $t0, computer_sign
+		jr $ra
+
+
+print_grid:
+	# wypisywanie planszy
+	li $v0, 4
+	la $a0, start_grid
+	syscall
+
+	li $v0, 4
+	la $a0, grid
+	syscall
+
+	jr $ra
+print_divider:
+	li $v0, 4
+	la $a0, divider
+	syscall
+
+	jr $ra
+get_rounds:
+	# pobieranie ilosci rund
     li $v0, 4
     la $a0, prompt_amount_rounds
     syscall
@@ -46,504 +110,25 @@ enter_amount_rounds:
     li $v0, 5
     syscall
 
-    blt $v0, 1, enter_amount_rounds # jesli ilosc rund jest mniejsza od 1 to powrot do wprowadzania ilosci rund
-    bgt $v0, 5, enter_amount_rounds # jesli ilosc rund jest wieksza od 5 to powrot do wprowadzania ilosci rund
+    blt $v0, 1, get_rounds # jesli ilosc rund jest mniejsza od 1 to powrot do wprowadzania ilosci rund
+    bgt $v0, 5, get_rounds # jesli ilosc rund jest wieksza od 5 to powrot do wprowadzania ilosci rund
     sw $v0, amount_rounds
 
-get_char:
-    # pobranie znaku gracza
-    li $v0, 4
+	jr $ra
+
+get_sign:
+	# pobranie znaku od gracza
+	li $v0, 4
     la $a0, choice_msg
     syscall
 
-    li $v0, 12
-    syscall
-    beq $v0, 'x', continue # jesli znak jest 'x' to kontynuacja
-    beq $v0, 'o', continue # jesli znak jest 'o' to kontynuacja
-    j get_char # jesli znak jest inny to powrot do pobierania znaku
-
-continue:
-    sb $v0, choiced # zapisanie wyboru gracza
-
-    # drukowanie linii oddzielajacej
-    li $v0, 4
-    la $a0, divider
-    syscall
-
-    # twozenie petli glownej
-    li $s0, 0 # licznik rund
-    main_loop:
-        beq $s0, amount_rounds, end # jesli licznik rund jest rowny ilosci rund to koniec gry
-        # pokazanie wiadmosci numerze rundy
-        li $v0, 4
-        la $a0, round_msg
-        syscall
-        # i+1 runda
-        li $v0, 1
-        addi $a0, $s0, 1
-        syscall
-
-        # czyszczenie tablicy
-        j clear_grid
-    after_clear_grid:
-        j game # wywolanie pojedynczej rozgrywki 
-    after_game:
-        j show_stats # pokazanie statystyk
-    after_show_stats:
-        addi $s0, $s0, 1 # zwiekszenie licznika rund o 1
-        j main_loop # powrot do petli glownej
-
-game:
-    # implementacja pojedynczej rozgrywki
-
-    jal display_grid # drukowanie tablicy
-        while:
-            jal check_win # sprawdzenie wygranej
-            beq $v0, 1, after_show_stats # jesli wygrana jest inna niz 0 to koniec rozgrywki
-            jal human_move
-            jal display_grid
-            j is_full
-        after_checking_is_full:
-            # wydrukowanie podzialki miedzy ruchami
-            li $v0, 4
-            la $a0, divider
-            syscall
-
-            j computer_move
-		after_computer_move:
-            jal display_grid
-            j while
-
-	# drukowanie linii oddzielajacej
-	li $v0, 4
-	la $a0, divider
+	li $v0, 12
 	syscall
-
-	# drukowanie tablicy
-	jal display_grid
-
-	# drukowanie wiadomosci o wygranej
-	# wywolanie funcji ktora zworoc nam znak ktory wygral
-
-
-	lb $t0, choiced
-
-
-	j after_game
-#Algorym ruchu komputera
-#-------------------------------------------
-
-computer_move:
-    lb $t0, choiced # pobranie wyboru gracza
-    beq $t0, 'x', set_computer_mark_on_o # jesli wybor gracza jest 'x' to ustawienie znaku komputera na 'o'
-    beq $t0, 'o', set_computer_mark_on_x # jesli wybor gracza jest 'o' to ustawienie znaku komputera na 'x'
-
-set_computer_mark_on_o:
-    li $t0, 'o'
-    sb $t0, computer_mark
-    j after_set
-
-set_computer_mark_on_x:
-    li $t0, 'x'
-    sb $t0, computer_mark
-    j after_set
-
-after_set:
-    # sprawdzenie czy mozna wygrac dokladajac jeden znak
-    li $t0, 0                  # $t0 - iterator i
-    li $t2, 0                  # $t2 - wartość do porównania z elementami tablicy grid
-
-    for_for_computer_move1:
-		beq $t0, 9, for_for_computer_move1_end # Jeśli i >= 9, zakończ pętlę
-		lb $t3, grid($t0)            # $t3 - pierwszy znak
-		# wartosc do porownania 
-		addi $t2, $t0, 1 # i+1
-		addu $t2, $t2, '0' # '0'+1+i 
-		beq $t3, $t2, make_computer_move1 # jesli znak jest rowny wartosci do porownania to powrot do petli
-		addi $t0, $t0, 1 # i++
-		j for_for_computer_move1
-
-    make_computer_move1:
-		# jesli znak jest inny niz wartosc do porownania to ustawienie znaku komputera na wartosc do porownania
-		lb $t4, computer_mark
-		sb $t4, grid($t0) # grid[i]=computer_mark
-		jal check_win # sprawdzenie wygranej
-		beq $v0, 1, after_computer_move # jesli wygrana jest inna niz 0 to koniec rozgrywki
-		# grid[i]='0'+i+1
-		sb $t2, grid($t0)
-		addi $t0, $t0, 1 # i++
-		j for_for_computer_move1
-
-	for_for_computer_move1_end:
-
-		#blokowanie gracza
-		li $t0, 0                  # $t0 - iterator i
-		li $t2, 0                  # $t2 - wartość do porównania z elementami tablicy grid
-
-		for_for_computer_move2:
-			beq $t0, 9, for_for_computer_move2_end # Jeśli i >= 9, zakończ pętlę
-			lb $t3, grid($t0)            # grid[i]
-			# wartosc do porownania
-			addi $t2, $t0, $t2 # i+1
-			addu $t2, $t2, '0' # '0'+1+i
-			beq $t3, $t2, for_for_computer_move2 # jesli znak jest rowny wartosci do porownania to powrot do petli
-			addi $t0, $t0, 1 # i++
-			j for_for_computer_move2
-
-		for_for_computer_move2:
-			# jesli znak jest inny niz wartosc do porownania to ustawienie znaku komputera na wartosc do porownania
-			lb $t4, choiced  # ladowanie znaku gracza
-			sb $t4, grid($t0) # zapisujemy znak gracza w tablicy
-			jal check_win # sprawdzenie wygranej
-			beq $v0, 1, for_for_computer_move_save_computer_mark # jesli wygrana jest inna niz 0 to koniec rozgrywki
-			# grid[i]='0'+i+1
-			sb $t2, grid($t0)            # $t2 - drugi znak
-			addi $t0, $t0, 1 # i++
-			j for_for_computer_move2
-
-	for_for_computer_move_save_computer_mark:
-			# grid[i]=computer_mark
-			lb $t4, computer_mark
-			sb $t4, grid($t0)
-			j  after_computer_move # wychodzimy z petli
-
-	for_for_computer_move2_end:
-
-			#sprobujmy zajmujac 5 pole 
-			lb $t0, grid(4)
-			beq $t0, '5', computer_move_save_mid # jesli pole jest zajete to zapisz znak komputera w tym polu
-			j skip_saving_mid
-
-	computer_move_save_mid:
-			# grid[4]=computer_mark
-			lb $t4, computer_mark
-			sb $t4, grid(4)
-			j after_computer_move # wychodzimy z petli
-
-	skip_saving_mid:
-
-			# sprobujemy zajac rog planszy 
-			li $t0, 0                  # $t0 - iterator i
-			corner_loop:
-				beq $t0, 4, corner_loop_end # Jeśli i >= 9, zakończ pętlę
-				lw $t2, corners($t0)            # corners[i]
-				lb $t3, grid($t2)            # grid[i]
-				# wartosc do porownania
-				addi $t2, $t0, $t2 # i+1
-				addu $t2, $t2, '0' # '0'+1+i
-				beq $t3, $t2, corner_loop_save_mark # jesli znak jest rowny wartosci do porownania to powrot do petli
-				addi $t0, $t0, 1 # i++
-				j corner_loop
-
-			corner_loop_save_mark:
-				# jesli znak jest inny niz wartosc do porownania to ustawienie znaku komputera na wartosc do porownania
-				lb $t4, computer_mark
-				sb $t4, grid($t2) # grid[i]=computer_mark
-				jal check_win # sprawdzenie wygranej
-				beq $v0, 1, after_computer_move # jesli wygrana jest inna niz 0 to koniec rozgrywki
-				addi $t0, $t0, 1 # i++
-				j corner_loop
-
-	corner_loop_end:
-
-			# sprobujmy zajac wolne pole
-			li $t0, 0                  # $t0 - iterator i
-			free_loop:
-				beq $t0, 9, free_loop_end # Jeśli i >= 9, zakończ pętlę
-				lb $t3, grid($t0)            # grid[i]
-				# wartosc do porownania
-				addi $t2, $t0, $t2 # i+1
-				addu $t2, $t2, '0' # '0'+1+i
-				beq $t3, $t2, free_loop_save_mark # jesli znak jest rowny wartosci do porownania to powrot do petli
-				addi $t0, $t0, 1 # i++
-				j free_loop
-
-	free_loop_save_mark:
-			lb $t4, computer_mark
-			sb $t4, grid($t0) # grid[i]=computer_mark
-			j after_computer_move # wychodzimy z petli
-
-is_full:
-    # sprawdzenie czy tablica jest pelna
-    li $t0, 0
-    is_full_loop:
-        beq $t0, 9, its_full
-        lb $t1, grid($t0)
-        beq $t1, 'x', is_full_loop
-        beq $t1, 'o', is_full_loop
-        j its_not_full
-
-    its_full:
-        li $v0, 1
-        li $a0, 2
-        syscall
-        j end_checking_is_full
-
-    its_not_full:
-        j after_checking_is_full
-
-end_checking_is_full:
-    # pokazanie wiadmosci koncowej
-    li $v0, 4
-    la $a0, draw_msg
-    syscall
-
-    j main_loop
-#-------------------------------------------
-human_move:
-    # pokazanie wiadmosci 
-    li $v0, 4
-    la $a0, prompt_position
-    syscall
-
-    li $v0, 5
-    syscall
-    blt $v0, 1, human_move
-    bgt $v0, 9, human_move
-    move $s3, $v0
-
-    li $t0, 0
-    is_proper_loop:
-         beq $t0, 9, its_proper
-         # obliczenie indeksu
-         sub $t2, $s3, 1
-         lb $t1, grid($t2)
-         beq $t1, 'x', human_move
-         beq $t1, 'o', human_move
-         addi $t0, $t0, 1
-         j is_proper_loop
-
-    its_proper:
-        sub $s3, $s3, 1
-        sb $s1, grid($s3)
-
-    # wyzerowanie rejestru
-    move $s3, $zero
-    jr $ra
-
-
-
-
-#-------------------------------------------
-check_win:
-    # implementacja sprawdzania wygranej
- li $t0, 0                 # $t0 - iterator pętli
-    la $t1, xoro              # $t1 - adresa tablicy xoro
-    lbu $t2, ($t1)            # $t2 - pierwszy znak 'x'
-    lbu $t3, 1($t1)           # $t3 - drugi znak 'o'
-    move $s2, $t2             # $s0 - mark (inicjalizacja pierwszym znakiem 'x')
-
-for_loop:
-    slti $t4, $t0, 2          # Sprawdź warunek i < 2
-    beq $t4, $zero, exit_loop # Jeśli i >= 2, zakończ pętlę
-
-    # Sprawdź wszystkie możliwe kombinacje zwycięstwa
-    la $t5, grid
-    lb $t6, ($t5)             # grid[0]
-    lb $t7, 1($t5)            # grid[1]
-    lb $t8, 2($t5)            # grid[2]
-    beq $t6, $s2, check_win_condition1
-    j check_win_condition2
-
-check_win_condition1:
-    beq $t7, $s2, check_win_condition1_2
-    j check_win_condition2
-
-check_win_condition1_2:
-    beq $t8, $s2, win_found
-
-check_win_condition2:
-    lb $t6, 3($t5)            # grid[3]
-    lb $t7, 4($t5)            # grid[4]
-    lb $t8, 5($t5)            # grid[5]
-    beq $t6, $s2, check_win_condition3
-    j check_win_condition4
-
-check_win_condition3:
-    beq $t7, $s2, check_win_condition3_2
-    j check_win_condition4
-
-check_win_condition3_2:
-    beq $t8, $s2, win_found
-
-check_win_condition4:
-    lb $t6, 6($t5)            # grid[6]
-    lb $t7, 7($t5)            # grid[7]
-    lb $t8, 8($t5)            # grid[8]
-    beq $t6, $s2, check_win_condition5
-    j check_win_condition6
-
-check_win_condition5:
-    beq $t7, $s2, check_win_condition5_2
-    j check_win_condition6
-
-check_win_condition5_2:
-    beq $t8, $s2, win_found
-
-check_win_condition6:
-    lb $t6, 0($t5)            # grid[0]
-    lb $t7, 3($t5)            # grid[3]
-    lb $t8, 6($t5)            # grid[6]
-    beq $t6, $s2, check_win_condition7
-    j check_win_condition8
-
-check_win_condition7:
-    beq $t7, $s2, check_win_condition7_2
-    j check_win_condition8
-
-check_win_condition7_2:
-    beq $t8, $s2, win_found
-
-check_win_condition8:
-    lb $t6, 1($t5)            # grid[1]
-    lb $t7, 4($t5)            # grid[4]
-    lb $t8, 7($t5)            # grid[7]
-    beq $t6, $s2, check_win_condition9
-    j check_win_condition10
-
-check_win_condition9:
-    beq $t7, $s2, check_win_condition9_2
-    j check_win_condition10
-
-check_win_condition9_2:
-    beq $t8, $s2, win_found
-
-check_win_condition10:
-    lb $t6, 2($t5)            # grid[2]
-    lb $t7, 5($t5)            # grid[5]
-    lb $t8, 8($t5)            # grid[8]
-    beq $t6, $s2, check_win_condition11
-    j check_win_condition12
-
-check_win_condition11:
-    beq $t7, $s2, check_win_condition11_2
-    j check_win_condition12
-
-check_win_condition11_2:
-    beq $t8, $s2, win_found
-
-check_win_condition12:
-    lb $t6, 0($t5)            # grid[0]
-    lb $t7, 4($t5)            # grid[4]
-    lb $t8, 8($t5)            # grid[8]
-    beq $t6, $s2, check_win_condition13
-    j check_win_condition14
-
-check_win_condition13:
-    beq $t7, $s2, check_win_condition13_2
-    j check_win_condition14
-
-check_win_condition13_2:
-    beq $t8, $s2, win_found
-
-check_win_condition14:
-    lb $t6, 2($t5)            # grid[2]
-    lb $t7, 4($t5)            # grid[4]
-    lb $t8, 6($t5)            # grid[6]
-    beq $t6, $s2, check_win_condition15
-    j increment_iterator
-
-check_win_condition15:
-    beq $t7, $s2, check_win_condition15_2
-    j increment_iterator
-
-check_win_condition15_2:
-    beq $t8, $s2, win_found
-
-increment_iterator:
-    addiu $t0, $t0, 1         # i++
-	move $s2, $t3 # zmiana znaku
-    j for_loop
-
-win_found:
-    li $v0, 1                 # Ustawienie wartości zwrotnej na true
-    jr $ra                    # Powrót z funkcji
-
-exit_loop:
-    li $v0, 0                 # Ustawienie wartości zwrotnej na false
-    jr $ra                    # Powrót z funkcji
-#-------------------------------------------
-
-display_grid:
-    # drukowanie tablicy
-    li $s1, 0
-    display_loop:
-        beq $s1, 9, after_display_end # jesli licznik jest rowny 9 to koniec drukowania
-        beq $s1, 3, display_new_line # jesli licznik jest rowny 3 to drukowanie nowej linii
-        beq $s1, 6, display_new_line # jesli licznik jest rowny 6 to drukowanie nowej linii
-        continue_displaying:
-        lb $a0, grid($s1) # pobranie komorki tablicy
-        li $v0, 11
-        syscall
-        addi $s1, $s1, 1 # zwiekszenie licznika o 1
-        j display_loop # powrot do petli drukujacej
-    after_display_end:
-        jr $ra
-    
-display_new_line:
-    # drukowanie nowej linii
-    li $v0, 4
-    la $a0, "\n"
-    syscall
-    j continue_displaying # powrot do petli drukujacej
-
-clear_grid:
-    # czyszczenie tablicy
-    li $s1, 0
-    clear_loop:
-        beq $s1, 9, after_clear_end # jesli licznik jest rowny 9 to koniec czyszczenia
-        sb $s1, grid($s1) # wyczyszczenie komorki tablicy
-        addi $s1, $s1, 1 # zwiekszenie licznika o 1
-        j clear_loop # powrot do petli czyszczacej
-
-show_stats:
-    # pokazanie statystyk
-    li $v0, 4
-    la $a0, divider
-    syscall
-
-    li $v0, 4
-    la $a0, player_name
-    syscall
-
-    li $v0, 4
-    la $a0, results_for_human
-    syscall
-
-    li $v0, 4
-    la $a0, computer
-    syscall
-
-    li $v0, 4
-    la $a0, results_for_computer
-    syscall
-
-    li $v0, 4
-    la $a0, divider
-    syscall
-
-    j after_show_stats # powrot do miejsca wywolania
-
-end:
-    # drukowanie linii oddzielajacej
-    li $v0, 4
-    la $a0, divider
-    syscall
-
-    # drukowanie wiadomosci koncowej
-    li $v0, 4
-    la $a0, end_msg
-    syscall
-
-    # drukowanie linii oddzielajacej
-    li $v0, 4
-    la $a0, divider
-    syscall
-
-    # koniec programu
-    li $v0, 10
-    syscall
-
-
-
+	beq $v0, 120, set_sign # jesli wybrano 'x' to ustaw znak gracza na 'x'
+	beq $v0, 111, set_sign # jesli wybrano 'o' to ustaw znak gracza na 'o'
+	j get_sign # jesli wybrano inny znak to powrot do wprowadzania znaku
+
+	set_sign:
+		sb $v0, choiced_sign
+		jr $ra
 
